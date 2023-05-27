a6f13bde7e071966edaa218c74f85be2b8fcb48e
Kenny <letronglam2632003@gmail.com>
2023-05-27 17:50:27 +0700

    InfoCourseHome

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Components/Banner/Banner'
+import InfoCourseHome from '../Components/InfoCourseHome/InfoCourseHome'
 
 export default function HomePage() {
     return (
         <div>
             <Banner/>
+            <InfoCourseHome/>
         </div>
     )
 }
